358db54c77f29d9456505d1d10770fa35d72ebcd
Ninaowl999 <128140719+Ninaowl999@users.noreply.github.com>
2023-11-07 21:30:05 +0400

    Commit add Task08.java.

---
+++ src/Task08.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Task08 {
+    public static void main(String[] args) {
+        System.out.println("Задание №8");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число N: ");
+        int N = scanner.nextInt();
+
+        int mask = N & (N - 1); // создаем маску для обнуления крайнего левого единичного бита
+        N &= ~mask; // обнуляем крайний левый единичный бит
+
+        System.out.println(Integer.toBinaryString(N)); // выводим результат в двоичном виде
+    }
+}
