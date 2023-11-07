e38db793dbb9eca0afb953e45729d8cc0c372e1d
Ninaowl999 <128140719+Ninaowl999@users.noreply.github.com>
2023-11-07 21:18:06 +0400

    Commit add Task05.java.

---
+++ src/Task05.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Task05 {
+    public static void main(String[] args) {
+        System.out.println("Задание №5");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число N: ");
+        int N = scanner.nextInt();
+        int i = scanner.nextInt();
+        int mask = ~(1 << i); // создаем маску для сброса i-го бита
+        N &= mask; // сбрасываем i-й бит
+        System.out.println(Integer.toBinaryString(N)); // выводим результат в двоичном виде
+    }
+}
