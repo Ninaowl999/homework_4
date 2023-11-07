dc39ffadb05c9223756a87aa07dba5cfd6e6a47a
Ninaowl999 <128140719+Ninaowl999@users.noreply.github.com>
2023-11-07 21:14:11 +0400

    Commit add Task04.java.

---
+++ src/Task04.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Task04 {
+    public static void main(String[] args) {
+        System.out.println("Задание №4");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число N: ");
+        int N = scanner.nextInt();
+        int i = scanner.nextInt();
+        int mask = 1 << i; // создаем маску для инвертирования i-го бита
+        N ^= mask; // инвертируем i-й бит
+        System.out.println(Integer.toBinaryString(N)); // выводим результат в двоичном виде
+    }
+}
